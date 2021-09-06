420d8b1dc8440b1f84dbbcb323427921e52a74d9
Matheus Henrique <matherique@gmail.com>
2021-09-05 22:57:34 -0300

    add some tests

--- test/create-user.test.ts
+++ test/create-user.test.ts
@@ -1,30 +1,53 @@
-type User = {
+type CreateUserParams = {
   firstName: string
   lastName: string
   email: string
 }
 
-interface CreateUser {
-  create: () => Promise<void>
+type UserModel = {
+  id: number
+  firstName: string
+  lastName: string
+  email: string
+  createdAt: Date
+  updateAt: Date
 }
 
-class CreateUserUsecase implements CreateUser {
+interface CreateUser {
+  create: (params: CreateUserParams) => Promise<UserModel>
+}
+
+class CreateUserUsecase {
   constructor(private readonly createUserRepository: CreateUserRepository) {}
 
-  async create(): Promise<void> {
-    return await this.createUserRepository.save()
+  async create(data: CreateUserParams): Promise<UserModel> {
+    const createdUser = await this.createUserRepository.save(data)
+
+    if (!createdUser) {
+      return null
+    }
+
+    return createdUser
   }
 }
 
+type CreateUserRepositoryParams = CreateUserParams
+
 interface CreateUserRepository {
-  save: () => Promise<void>
+  save: (data: CreateUserRepositoryParams) => Promise<UserModel | null> 
 }
+
 
 class CreateUserRepositorySpy implements CreateUserRepository {
   public saveCount: number = 0;
+  public saveParams: CreateUserRepositoryParams
+  public saveReturn: UserModel
 
-  async save(): Promise<void> {
+  async save(data: CreateUserRepositoryParams): Promise<UserModel | null> {
     this.saveCount++;
+    this.saveParams = data
+
+    return this.saveReturn
   }
 }
 
@@ -36,11 +59,51 @@ function makeSut() {
 }
 
 describe("Create user", () => {
+  const userData = {
+    firstName: 'any_firstname',
+    lastName: 'any_lastname',
+    email: 'any_email@email.com',
+  }
+
   it("should call save on sut.create", async () => {
     const { repositorySpy, sut } = makeSut()
 
-    await sut.create()
+    await sut.create(userData)
     expect(repositorySpy.saveCount).toBe(1)
+  })
+  
+  it("should call save with corret params", async () => {
+    const { repositorySpy, sut } = makeSut()
+    await sut.create(userData)
+    expect(repositorySpy.saveCount).toBe(1)
+    expect(repositorySpy.saveParams).toEqual(userData)
+  })
+
+  it("should return a new user with corret params", async () => {
+    const { repositorySpy, sut } = makeSut()
+
+    repositorySpy.saveReturn = { 
+      ...userData, 
+      id: 1, 
+      createdAt: new Date(),
+      updateAt: new Date() 
+    }
+
+    const result = await sut.create(userData)
+    expect(repositorySpy.saveCount).toBe(1)
+    expect(repositorySpy.saveReturn).toEqual(result)
+  })
+
+  it("should validate and return null if email alreaby been used", async () => {
+    const { repositorySpy, sut } = makeSut()
+
+    jest.spyOn(repositorySpy, 'save').mockImplementation(() => {
+      repositorySpy.saveCount += 1
+      return null
+    })
+
+    const result = await sut.create(userData)
+    expect(result).toBeNull()
   })
 
 })
