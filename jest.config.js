2f88ebec5a5c2d00f432ca3d1ed41ba0cb58cf61
Matheus Henrique <matherique@gmail.com>
2021-09-07 23:04:38 -0300

    change jest configuration

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,27 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  resetMocks: true,
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  coverageReporters: [
+    "text",
+    "lcov"
+  ],
+  testEnvironment: "node",
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    }
+  },
+  watchPathIgnorePatterns: ["node_modules"],
+  transformIgnorePatterns: ["node_modules"],
+  collectCoverageFrom: [
+    "src/**/*.ts"
+  ]
 };
