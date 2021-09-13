d62ef4c5e808dc25a0069ac76d9002321d34ae82
Matheus Henrique <matherique@gmail.com>
2021-09-13 00:45:47 -0300

    change mock to spy on jwt package

--- src/infra/token/jwt.test.ts
+++ src/infra/token/jwt.test.ts
@@ -1,21 +1,14 @@
 import { JWT } from "./jwt"
 import jsonwebtoken from 'jsonwebtoken'
 
-jest.mock('jsonwebtoken', () => {
-  return {
-    sign: jest.fn()
-  }
-})
-
-const mockedJsonwebtoken = jsonwebtoken as jest.Mocked<typeof jsonwebtoken>
-
 describe("Jsonwebtoken", () => {
   const secret = 'secret'
 
   test("should return a valid token", async () => {
     const jwt = new JWT(secret)
     const fakeToken = "random_token"
-    mockedJsonwebtoken.sign.mockImplementationOnce(() => fakeToken)
+
+    jest.spyOn(jsonwebtoken, 'sign').mockImplementationOnce(() => fakeToken)
     const token = await jwt.create({ random: 'payload' }, 1)
 
     expect(token).toBe(fakeToken)
@@ -31,10 +24,9 @@ describe("Jsonwebtoken", () => {
       options: { expiresIn: 1 }
     }
 
-    mockedJsonwebtoken.sign.mockImplementationOnce(() => fakeToken)
+    jest.spyOn(jsonwebtoken, 'sign').mockImplementationOnce(() => fakeToken)
     await jwt.create(params.payload, params.options.expiresIn)
-
     
-    expect(mockedJsonwebtoken.sign).toHaveBeenCalledWith(...Object.values(params))
+    expect(jsonwebtoken.sign).toHaveBeenCalledWith(...Object.values(params))
   })
 })
