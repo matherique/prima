d38f15102e42a21d9d5dec1e44b418078fdcf423
Matheus Henrique <matherique@gmail.com>
2021-11-27 16:34:29 -0300

    fix code format

--- src/modules/user/usercases/create-user.ts
+++ src/modules/user/usercases/create-user.ts
@@ -1,6 +1,9 @@
-import { CreateUserParams } from "../domain/usecases/create-user"
-import { CreateUserRepository, CreateUserRepositoryResult } from "./ports/create-user-repository"
-import { HashingService } from "./ports/hashing-service"
+import { CreateUserParams } from "../domain/usecases/create-user";
+import {
+  CreateUserRepository,
+  CreateUserRepositoryResult,
+} from "./ports/create-user-repository";
+import { HashingService } from "./ports/hashing-service";
 
 export class CreateUserUsecase {
   constructor(
@@ -9,17 +12,17 @@ export class CreateUserUsecase {
   ) {}
 
   async create(data: CreateUserParams): Promise<CreateUserRepositoryResult> {
-    const hashedPassword = await this.hashingService.encode(data.password)
+    const hashedPassword = await this.hashingService.encode(data.password);
 
     const createdUser = await this.createUserRepository.save({
-      ...data, password: hashedPassword 
-    })
+      ...data,
+      password: hashedPassword,
+    });
 
     if (!createdUser) {
-      return null
+      return null;
     }
 
-    return createdUser
+    return createdUser;
   }
 }
-
